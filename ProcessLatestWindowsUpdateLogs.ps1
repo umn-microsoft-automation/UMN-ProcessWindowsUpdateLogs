6868a90de4e703ff2486e2e0f06085ee04033f10
Travis Sobeck <nex916@gmail.com>
2018-04-26 12:47:25 -0500

    Add files via upload
    Needs a bit of work

---
+++ ProcessLatestWindowsUpdateLogs.ps1
@@ -0,0 +1,137 @@
+# ProcessLatestWindowsUpdateLogs.ps1
+# By Craig Woodford (craigw@umn.edu)
+# Last update 4/4/2018
+
+# This script is used to convert Windows Update logs for Server 2016 systems
+# into text-based logs for ingestion into a SIEM system such as Splunk. The
+# script is designed to run on a regular basis so that only logs that have not
+# been converted since the last script run-time are processed. The script needs
+# to be run with administrative rights.
+
+# Windows update ETL log files location (default: C:\Windows\Logs\WindowsUpdate).
+$logPath = "C:\Windows\Logs\WindowsUpdate"
+
+# Path of the file that will contain the last run time.
+$runTimePath = "C:\Scripts\powershell-scratch\fileread_example.txt"
+
+# Path for the Windows update log file (this file will be overwritten with each run).
+$outputPath = "C:\Scratch\WindowsUpdate.log"
+
+# Windows Event Log source name.
+$eventLogSourceName = "MPT-GetLatestWindowsUpdateLogs.ps1"
+
+# Windows build number - the script will only run for build numbers greater then provided.
+# The windows 10 RTM build number is 10240.
+$earliestBuildtoRun = 10240
+
+function Invoke-ProcessLatestWindowsUpdateLogs {
+<#
+.Synopsis
+   Builds the Windows update logs since the last time the function ran.
+.DESCRIPTION
+   Builds the Windows update logs from a time specified in a file and then
+   updates that file to contain the latest run time. This function is intended
+   to be run to automate the collection of the Windows update logs into a SIEM
+   solution such as Splunk. This function leverages the Get-WindowsUpdateLog
+   cmdlet to convert .etl log files into a text-based log file. This function
+   should be executed by an account with administrative rights.
+.EXAMPLE
+   Invoke-GetLatestWindowsUpdateLogs -etlDirectoryPath "C:\Windows\Logs\WindowsUpdate" `
+    -lastRunTimeFilePath "C:\WULogScript\lastruntime.txt" `
+    -LogOutputSource "C:\WULogScript\WindowsUpdate.log" `
+    -eventLogSource "GetLatestWindowsUpdateLogs"
+.etlDirectoryPath
+   The path to the directory containing the Windows update .etl log files.
+   The default location is: C:\Windows\Logs\WindowsUpdate
+.lastRunTimeFilePath
+   The location of the file containing a timestamp of the last time the function ran. The file
+   should only contain the timestamp (generated using (Get-Date).toString()). The file will be
+   overwritten as a part of the function. If the file is not present, the user executing the
+   function does not have rights to the file or if the file data is invalid then a date of
+   1/1/2018 will be assumed and all Windows update logs written after that date will be processed.
+.logOutputPath
+   The path for the text-based Windows update log to be written to. This file will be overwritten 
+   the next time the function runs.
+.eventLogSource
+   The name of the Windows Event Log source that will be instantiated in the Windows Application 
+   Event Log. This is used to log the function's actions into a log that is assumed to be 
+   ingested by a SIEM solution.
+#>
+    Param (
+        [string]$etlDirectoryPath,
+        [string]$lastRunTimeFilePath,
+        [string]$logOutputPath,
+        [string]$eventLogSource
+    )
+
+    try {
+        # Test if the Windows Application Event Log source has been instantiated yet.        
+        if(-not [System.Diagnostics.EventLog]::SourceExists($eventLogSource)) {
+            # Instantiate the log source.
+            New-EventLog -LogName Application -Source $eventLogSource
+        }
+    }
+    catch {
+        # Depending on the rights of the using invoking the function the previous
+        # test may throw an error which just indicates that the Log source is not
+        # present.
+        New-EventLog -LogName Application -Source $eventLogSource
+    }
+
+    try {
+        # Test is if the last run time file is valid.
+        if(Test-Path -Path $lastRunTimeFilePath) {
+        
+             try {
+                # Get the content of the last run time file and convert it into a DateTime.
+                $fileTimeContent = Get-Content -Path $lastRunTimeFilePath
+                $lastRunTime = [DateTime]$fileTimeContent
+             }
+             catch {
+                # If there is a problem accessing the run time file or if the data does not convert into a DateTime then set the last run time to 1/1/18 and log this.
+                $lastRunTime = Get-Date -Date '1/1/18'
+                Write-EventLog -LogName Application -Source $eventLogSource -EventId 11666 -Message "Unable to access $lastRunTimeFilePath or bad last run time."
+             }
+        }
+        else {
+            # If the last run time file doesn't exist set the last run time to 1/1/18 and log this.
+            $lastRunTime = Get-Date -Date '1/1/18'
+            Write-EventLog -LogName Application -Source $eventLogSource -EventId 11667 -Message "Last run time file $lastRunTimeFilePath does not exist."
+        }
+
+        # Get all files with modified times greater then the last run time.
+        $filesToCheck = Get-ChildItem -Path $etlDirectoryPath -File | where {$_.LastWriteTime -gt $lastRunTime}
+
+        # If there are files to check then run Get-WindowsUpdateLog against them.
+        if($filesToCheck) {
+            $filesToCheck | Get-WindowsUpdateLog -LogPath $logOutputPath
+            $fileCheckCount = $filesToCheck.Count
+        }
+        else {
+            $fileCheckCount = 0
+        }
+
+        # Set the current run time and update the last run time file with the updated timestamp.
+        $currentRunTime = (Get-Date).ToString()
+        Set-Content -Path $lastRunTimeFilePath -Value $currentRunTime -Force
+
+        # Update the Windows Application Event Log with function run information.
+        Write-EventLog -LogName Application -Source $eventLogSource -EventId 11660 -Message "GetLatestWindowsUpdateLogs ran: $fileCheckCount files. Previous run time was: $lastRunTime"
+
+        return
+    }
+    catch {
+        Write-EventLog -LogName Application -Source $eventLogSource -EventId 11668 -Message $_.Exception
+        throw $_
+    }
+
+}
+
+# Run the script only on systems with OS build numbers greater then $earliestBuildtoRun.
+if([Environment]::OSVersion.Version.Build -gt $earliestBuildtoRun) {
+    Invoke-ProcessLatestWindowsUpdateLogs -etlDirectoryPath $logPath -lastRunTimeFilePath $runTimePath -logOutputPath $outputPath -eventLogSource $eventLogSourceName
+    return
+}
+else {
+    return
+}
